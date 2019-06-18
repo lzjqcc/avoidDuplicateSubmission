f432d372dd2ac4c417a6b1e7c4fe5c8e74a707cd
lizhijian <lizhijian@mytian.com>
2019-06-18 18:05:20 +0800

    注视

--- src/main/java/com/example/avoidDuplicateSubmission/enums/TokenTypeEnum.java
+++ src/main/java/com/example/avoidDuplicateSubmission/enums/TokenTypeEnum.java
@@ -1,10 +1,7 @@
 package com.example.avoidDuplicateSubmission.enums;
 
 public enum  TokenTypeEnum {
-    /**
-     * 不刷新的情况下防止重复提交
-     */
-    SimplePreventingCommit,
+
     /**
      * 包含刷新的情况下防止重复提交
      */
